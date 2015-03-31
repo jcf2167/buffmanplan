b6fdae49a58ed35cde1486d4f02d0ff98b2fe1be
jessica fan <jcf2167@columbia.edu>
2015-03-31 02:37:36 -0400

    fin front page

--- index.php
+++ index.php
@@ -2,17 +2,62 @@
 <html>
 
 <head>
-    <title>What's Up Guys</title>
-    <meta name="description" content="alpaca farm lol">
-    <link rel="stylesheet" type="text/css" href="skeleton.css">
+	 <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Buffmanplan</title>
+    <meta name="description" content="buffmanplan">
+    <link rel="stylesheet" type="text/css" href="static/bootstrap.css">
+      <!-- Custom styles for this template -->
+    <link href="static/cover.css" rel="stylesheet">
 </head>
 
 <body background="stardust.png">
 	
 
-    <div id- "container" align="center">
-        testing. 
-</div>
+   <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">B U F F M A N P L A N </h3>
+              <nav>
+                <ul class="nav masthead-nav">
+                  <li class="active"><a href="index.php">Home</a></li>
+                  <li><a href="features.php">Features</a></li>
+                  <li><a href="contact.php">Contact</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Want to get fit?</h1>
+            <p class="lead">Get mad buff for no reason!</p>
+            <p class="lead">
+              <a href="#" class="btn btn-lg btn-default">Login</a>
+               <a href="#" class="btn btn-lg btn-default">Sign Up</a>
+            </p>
+          </div>
+
+          
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 </body>
 
 </html>
