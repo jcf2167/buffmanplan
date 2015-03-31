30beb607f53d42c4bbc688e22d70003883534777
jessica fan <jcf2167@columbia.edu>
2015-03-31 03:30:09 -0400

    change links

---
+++ navbar.php
@@ -0,0 +1,12 @@
+<div class="masthead clearfix">
+                    <div class="inner">
+                        <h3 class="masthead-brand">B U F F M A N P L A N </h3>
+                        <nav>
+                            <ul class="nav masthead-nav">
+                                <li class="active"><a href="index.php">Home</a></li>
+                                <li><a href="features.php">Features</a></li>
+                                <li><a href="contact.php">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
