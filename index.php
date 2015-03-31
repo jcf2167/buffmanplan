621d6d14fa0eccee2a05cd096c58c267efebdbb9
jessica fan <jcf2167@columbia.edu>
2015-03-31 02:05:20 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<html>
+
+<head>
+    <title>What's Up Guys</title>
+    <meta name="description" content="alpaca farm lol">
+    <link rel="stylesheet" type="text/css" href="skeleton.css">
+</head>
+
+<body background="stardust.png">
+	
+
+    <div id- "container" align="center">
+        testing. 
+</div>
+</body>
+
+</html>
