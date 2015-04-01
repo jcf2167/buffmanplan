ca6e85522414f2aa96047b05e4c1a377bf1cf0c8
jessica fan <jcf2167@columbia.edu>
2015-03-31 21:22:09 -0400

    added stuff

---
+++ index2.php
@@ -0,0 +1,24 @@
+<?php
+	ini_set('display_errors', 'On');
+
+	// Create connection 
+	$dburl = "cs4111.c1xwtu16srrr.us-east-1.rds.amazonaws.com";
+	$dbuser = "jcf2167";
+	$dbpassword = "mycatisdead";
+	$dbname = "cs4111";
+	$conn = mysqli_connect($dburl,$dbuser,$dbpassword,$dbname, 3306);
+
+	// Check connection
+	if (mysqli_connect_errno()) {
+	  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	} else {
+		$result = mysqli_query($conn, "SHOW TABLES");
+
+		while($row = mysqli_fetch_array($result)) {
+			echo $row[0];
+			echo "<br>";
+		}
+
+		mysqli_close($conn);
+	}
+?>
