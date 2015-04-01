aa21401e550b1f9bec839546babd17497115cc97
jessica fan <jcf2167@columbia.edu>
2015-04-01 13:40:16 -0400

    aws updated their rds requirements

--- login-form.php
+++ login-form.php
@@ -6,7 +6,13 @@
 	$dbuser = "jcf2167";
 	$dbpassword = "mycatisdead";
 	$dbname = "cs4111";
-	$conn = mysqli_connect('cs4111.c1xwtu16srrr.us-east-1.rds.amazonaws.com','jcf2167','mycatisdead','cs4111');
+
+	$dburl = "cs4111temp.c1xwtu16srrr.us-east-1.rds.amazonaws.com";
+	$dbuser = "jcf2167";
+	$dbpassword = "mycatisdead";
+	$dbname = "cs4111temp";
+
+	$conn = mysqli_connect($dburl,$dbuser,$dbpassword,$dbname);
 
 	// Check connection
 	if (mysqli_connect_errno()) {
@@ -15,8 +21,8 @@
 		$result = mysqli_query($conn, "SHOW TABLES");
 
 		while($row = mysqli_fetch_array($result)) {
-			echo $row[0];
-			echo "<br>";
+			echo $row[0];  
+			echo "<br>"; 
 		}
 
 		mysqli_close($conn);
