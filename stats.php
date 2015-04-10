da031b9ea34e43a6fbb3c30db1dbdc006d70a77c
jessica fan <jcf2167@columbia.edu>
2015-04-10 00:00:53 -0400

    working on mealplan

--- stats.php
+++ stats.php
@@ -1,12 +1,99 @@
 <?php
 session_start();
+
+$goal=$_SESSION['goal'];
+$weight=$_SESSION['weight'];
+$activitylevel = $_SESSION['activitylevel'];
+$bodyfat = $_POST['bodyfat'];
+$calories = 0;
+$carbs = 0;
+$protein = 0;
+$fat = 0;
+$constraints = $_SESSION["constraints"];
+
+
+$constraintsmap = array(
+		0 => "None" , 
+		1 =>"Vegetarian",
+		2 =>"Nuts",
+		3 =>"Lactose",
+		4 =>"Fish",
+		5 =>"Pork",
+		6 =>"Beef"
+
+);
+
+$constraints = $constraintsmap[$constraints];
+
+function getCalories($n){
+	return $_SESSION['weight'] * ($_SESSION['activitylevel'] + 10) + $n;
+}
+function getCPF($calories,$n,$g){
+	return ($n * $calories)/$g;
+}
+
+#maintain
+if($goal==0 || $goal==3){
+	$calories = getCalories(0);
+	$carbs = getCPF($calories, .3,4);
+	$protein= getCPF($calories, .4, 4);
+	$fat = getCPF($calories, .3, 8);
+}
+
+#lose
+else if ($goal==1){
+	$calories = getCalories(-500);
+	$carbs = getCPF($calories, .20,4);
+	$protein= getCPF($calories, .50,4);
+	$fat = getCPF($calories, .30,8);
+
+
+}
+
+#gain
+else if($goal ==2){
+	$calories = getCalories(250);
+	$carbs = getCPF($calories, .40,4);
+	$protein= getCPF($calories, .40,4);
+	$fat = getCPF($calories, .20,8);
+}
+
+#bodyBUILD
+else {
+	$calories = getCalories(500);
+	$carbs = getCPF($calories, .40,4);
+	$protein= $weight/4;
+	$fat = getCPF($calories, .20,8);
+}
+
+	$dburl = "cs4111temp.c1xwtu16srrr.us-east-1.rds.amazonaws.com";
+	$dbuser = "jcf2167";
+	$dbpassword = "mycatisdead";
+	$dbname = "cs4111temp";
+	$conn = mysqli_connect($dburl,$dbuser,$dbpassword,$dbname);
+	if (mysqli_connect_errno()) {
+		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		} else {
+
+			#echo "success";
+			$sql = "select * from Food where ".$constraints."=0";
+			echo $sql;
+			$result = mysqli_query($conn,$sql);
+
+			while($row = $result->fetch_assoc()) {
+		        echo "hi";
+		    }
+
+
+			}
+
 ?>
 
 <html>
+
 <?php include "header.php" ?>
 
 <body background="img/background.png">
-
 
     <div class="site-wrapper">
         <div class="site-wrapper-inner">
@@ -20,8 +107,15 @@ session_start();
 
                     <h1 class="cover-heading">Welcome 
                     <?php
-					echo $_SESSION['email'];
+					echo $_SESSION['constraints'];
 					?>!</h1>
+					<h4>Total Calories Needed: <?php echo $calories ?> </h4>
+					<h4>Total Carbs Needed: <?php echo $carbs ?> </h4> 
+					<h4>Total Fats Needed: <?php echo $fat ?> </h4> 
+
+					<h4>Total Protein Needed: <?php echo $protein ?> </h4> 
+
+ 
 
                     <table class="table table-hover">
 				    <thead>
